6ecd90307affbfdbf1813aa99eb9de2b50a92447
Carlos Eduardo <carloseduardokf@gmail.com>
2024-12-10 16:11:04 -0300

    creating code and its functions

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,39 @@
+let dolar = 5.1
+
 let usdInput = document.querySelector("#usd")
 let brlInput = document.querySelector("#brl")
 
 usdInput.addEventListener('keyup', () => {
-    console.log('apertou no campo USD')
+
 })
 
 brlInput.addEventListener('keyup', () => {
-    console.log('aprtou  no campo BRL')
+
 })
 
+usdInput.value = '1000,00'
+convert('usd-to-brl')
+
+function formatCurrency (value) {
+
+}
+
+function fixValue(value) {
+    let fixedValue = value.replace(',', '.')
+    let floatValue = parseFloat(fixedValue)
+
+    if (floatValue == NaN) {
+        floatValue = 0
+    }
+    return floatValue
+}
+
+function convert(type) {
+    if (type == "usd-to-brl") {
+
+    }
+
+    if (type == 'brl-to-usd') {
+
+    }
+}
