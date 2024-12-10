13ceb99a6e5b8fc290e13b84a971aa7af53cda98
Carlos Eduardo <carloseduardokf@gmail.com>
2024-12-10 17:23:29 -0300

    finishing the job with pure javascript

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,37 @@
-let dolar = 5.1
+let dolar = 6.05
 
 let usdInput = document.querySelector("#usd")
 let brlInput = document.querySelector("#brl")
 
 usdInput.addEventListener('keyup', () => {
-
+    convert('usd-to-brl')
 })
 
 brlInput.addEventListener('keyup', () => {
+    convert('brl-to-usd')
+})
 
+usdInput.addEventListener('blur', () => {
+    usdInput.value = formatCurrency(usdInput.value)
+})
+
+brlInput.addEventListener('blur', () => {
+    brlInput.value = formatCurrency(brlInput.value)
 })
 
 usdInput.value = '1000,00'
 convert('usd-to-brl')
 
 function formatCurrency (value) {
+    let fixedValue = fixValue(value)
+    let options = {
+        useGrouping: false,
+        minimumFractionDigits: 2,
+    }
 
+    let formatter = new Intl.NumberFormat('pt-BR', options)
+
+    return formatter.format(fixedValue)
 }
 
 function fixValue(value) {
@@ -30,10 +46,20 @@ function fixValue(value) {
 
 function convert(type) {
     if (type == "usd-to-brl") {
+        let fixedValue = fixValue(usdInput.value)
 
+        let result = fixedValue * dolar
+        result = result.toFixed(2)
+
+        brlInput.value = formatCurrency(result)
     }
 
     if (type == 'brl-to-usd') {
+        let fixedValue = fixValue(brlInput.value)
 
+        let result = fixedValue / dolar
+        result = result.toFixed(2)
+
+        usdInput.value = formatCurrency(result)
     }
 }
